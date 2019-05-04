28bfdeb07314898680590ab3d6f685589beb1f5c
joesus <joesusnick@gmail.com>
2019-05-04 15:16:53 -0700

    Added unconfigured framework target

--- NoExceptions/NoExceptions.h
+++ NoExceptions/NoExceptions.h
@@ -2,13 +2,18 @@
 //  NoExceptions.h
 //  NoExceptions
 //
-//  Created by CatPalace on 4/27/19.
-//  Copyright © 2019 CatPalace. All rights reserved.
+//  Created by Joe Susnick on 5/4/19.
+//  Copyright © 2019 Joe Susnick. All rights reserved.
 //
 
-#ifndef NoExceptions_h
-#define NoExceptions_h
+#import <UIKit/UIKit.h>
 
-#import "NSExceptionError.h"
+//! Project version number for NoExceptions.
+FOUNDATION_EXPORT double NoExceptionsVersionNumber;
 
-#endif /* NoExceptions_h */
+//! Project version string for NoExceptions.
+FOUNDATION_EXPORT const unsigned char NoExceptionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoExceptions/PublicHeader.h>
+
+
