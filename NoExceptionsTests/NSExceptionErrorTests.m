ce136c7ec07b1171cfed27ca9ac941bf044a4d2f
joesus <joesusnick@gmail.com>
2019-05-04 15:48:54 -0700

    Correcting equality in unit tests

--- NoExceptionsTests/NSExceptionErrorTests.m
+++ NoExceptionsTests/NSExceptionErrorTests.m
@@ -16,8 +16,8 @@
 @implementation NSExceptionErrorTests
 
 - (void)testNoExceptionsErrorDomain {
-    XCTAssertEqual(NoExceptionsErrorDomain, @"NoExceptionsErrorDomain",
-                   @"There should be a public symbol for the error domain");
+    XCTAssertEqualObjects(NoExceptionsErrorDomain, @"NoExceptionsErrorDomain",
+                          @"There should be a public symbol for the error domain");
 }
 
 - (void)testCreatingWithException {
@@ -31,15 +31,15 @@
 
     NSExceptionError *error = [[NSExceptionError alloc] initWithException:exception];
 
-    XCTAssertEqual(error.domain, NoExceptionsErrorDomain,
-                   @"An exception wrapping error should have a domain associated with it");
+    XCTAssertEqualObjects(error.domain, NoExceptionsErrorDomain,
+                          @"An exception wrapping error should have a domain associated with it");
     XCTAssertEqual(error.code, 0,
                    @"An exception wrapping error should have a code of zero");
-    XCTAssertEqual(error.userInfo, userInfo,
-                   @"The exception's user info dictionary should be accessible through the error's dictionary");
+    XCTAssertEqualObjects(error.userInfo, userInfo,
+                          @"The exception's user info dictionary should be accessible through the error's dictionary");
 
-    XCTAssertEqual(error.exception, exception,
-                   @"The original exception should be included as a property of the error");
+    XCTAssertEqualObjects(error.exception, exception,
+                          @"The original exception should be included as a property of the error");
 }
 
 @end
