a4d171b404e1a0c6916638e117b72ea4f8dea97e
joesus <joesusnick@gmail.com>
2019-05-04 15:50:47 -0700

    Updating headers

--- NoExceptions/NoExceptions.h
+++ NoExceptions/NoExceptions.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char NoExceptionsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NoExceptions/PublicHeader.h>
 
-
+#import <NoExceptions/ExceptionTrapper.h>
+#import <NoExceptions/NSExceptionError.h>
