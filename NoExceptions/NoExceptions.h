b71c691f5f129c95c7a251be9fd57a809510cd09
joesus <joesusnick@gmail.com>
2019-05-18 17:47:53 -0700

    Adding mac os

--- NoExceptions/NoExceptions.h
+++ NoExceptions/NoExceptions.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Joe Susnick. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for NoExceptions.
 FOUNDATION_EXPORT double NoExceptionsVersionNumber;
